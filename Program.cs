5f66371c144802a74a7bed3858c11eaed0e12027
Anna <anka-kudrow@mail.ru>
2023-07-10 08:16:20 +0300

    added 13 bootcamp

--- Program.cs
+++ Program.cs
@@ -1,1 +1,158 @@
 ﻿
+
+//                      АРИФМЕТИЧЕСКАЯ ПРОГРЕССИЯ
+
+
+// Напишите программу которая принимает на вход число N и возвращает сумму чисел от 1 до N.
+// Решение при помощи арифметической прогрессии, сложность - O(1) константный алгоритм:
+
+// Console.Clear();
+// Console.WriteLine("Input a number: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(((1 + n) / 2.0) * n); 
+
+// Решение при помощи цикла, линейный алгоритм О(n):
+
+// Console.Clear();
+// System.Console.WriteLine("Input a number: ");
+// int n = int.Parse(Console.ReadLine()!), result = 0;
+// for (int i = 0; i <= n; i++)
+// {
+//     result += i;
+// }
+// System.Console.WriteLine($"Summ of numbers from 1 to {n} = {result}");
+                
+
+
+                    // СОРТИРОВКА ПУЗЫРЬКОМ, квадратичный алгоритм О(n ** 2)
+
+
+
+// Console.Clear();
+// System.Console.WriteLine("Input size of array: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int [] array = new int[n];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(-20,21);
+// }
+// System.Console.WriteLine($"Initial array: [{string.Join(", ", array)}]");
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     for (int j = 0; j < array.Length - 1; j++)
+//     {
+//         if(array[j] < array[j + 1])(array[j], array[j + 1]) = (array[j + 1], array[j]);
+//     }       
+// }
+// System.Console.WriteLine($"End array:  [{string.Join(", ", array)}]");
+
+// Последовательность Фибоначчи Рекурсией:
+// int fib(int n)
+// {
+//     if (n == 0 || n == 1)
+//     return 1;
+//     return fib (n - 1) + fib (n - 2);
+// }
+
+// Console.Clear();
+// System.Console.Write("Input a number: ");
+// int n = int.Parse(Console.ReadLine()!);
+// System.Console.WriteLine(fib(n));
+
+// Последовательность Фибоначчи циклом:
+
+// System.Console.Write("Input a number: ");
+// int n = int.Parse(Console.ReadLine()!), a0 = 0, a1 = 1, x;
+// for (int i = 0; i < n; i++)
+// {
+//     x = a0 + a1;
+//     a0 = a1;
+//     a1 = x;
+// }
+
+// System.Console.WriteLine(a0);
+
+
+
+                            // СОРТИРОВКА ВЫБОРОМ
+
+
+// int [] SortSelection (int [] collection)      
+// {
+//     int size = collection.Length;
+//     for (int i = 0; i < size - 1; i++)
+//     {
+//         int pos = i;
+//         for (int j = i + 1; j < size; j ++)
+//         {
+//             if (collection[j] < collection[pos])
+//             pos = j;
+
+//         }
+//         int temp = collection[i];
+//         collection[i] = collection[pos];
+//         collection[pos] = temp;
+//     }
+//     return collection;
+// }
+// var arr = new int [] {9, 6, 0, 5, 7, 3, 1, 2};
+// System.Console.WriteLine(String.Join(' ', arr));    
+// SortSelection(arr);
+// System.Console.WriteLine(String.Join(' ', arr));   
+
+
+
+//                     СОРТИРОВКА ПОДСЧЁТОМ
+
+                    //    ДЛЯ ЦИФР (0-9):
+
+int[] array = {0,2,3,2,1,5,9,1,1};
+
+CountingSort(array);
+
+System.Console.WriteLine(string.Join(", ", array));
+
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10];         // создаём второй массив для записи повторений цифр
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+         counters[inputArray[i]]++;       // считаем количество повторений цифр(превращаем элемент первого массива в индекс второго массива как бы и делаем в этом индексе +1)
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)  //обход второго  массива повторений 
+    {
+        for (int j = 0; j < counters[i]; j++) // вложенный цикл для
+        {
+            inputArray[index] = i; // 
+            index++;
+        }
+    }
+}
+
+
+        //   ДЛЯ ЧИСЕЛ (РАСШИРЕННЫЙ МЕТОД):
+
+void CountingSortExtended (int[] inputArray)
+{
+    int max = inputArray.Max();
+
+    int[] counters = new int [max + 1];
+
+    for (int i = 0; i < inputArray; i++)
+    {
+        co 
+    }
+}
+
+
+
+
+
+
+
+
+
+
