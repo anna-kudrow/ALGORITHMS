36ab8fa920edd44a5c69482b2e5aa66f8fcf505c
Anna <anka-kudrow@mail.ru>
2023-08-03 13:27:04 +0300

    added a python file

--- Recursion.py
+++ Recursion.py
@@ -42,4 +42,12 @@
 
 #  фибоначчи
 
-def fibonacci
+# def fibonacci(n):
+#     if n == 0: 
+#         return 0
+#     elif n == 1: 
+#         return 1
+#     else:
+#         return fibonacci(n-1) + fibonacci(n -2)
+    
+# print(fibonacci(7))     
